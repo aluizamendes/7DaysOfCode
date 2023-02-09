1a324bae655880ce551f408e5076ba4248bda164
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-02-08 21:46:28 -0300

    refactoring menu mobile

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 function openMenu() {
-
-	let menuMobile = document.querySelector('.menu-mobile-links');
-
-	document.querySelector('.icon-menu-hamburger').src="assets/close_FILL0_wght400_GRAD0_opsz48.svg"
-
-	if (menuMobile.classList.contains('open-menu')) {
-		menuMobile.classList.remove('open-menu')
-
-		document.querySelector('.icon-menu-hamburger').src="assets/menu_FILL0_wght400_GRAD0_opsz48.svg"
-	}
-
-	else {
-		menuMobile.classList.add('open-menu')
-	}
+  let menuLinks = document.querySelector(".nav-menu");
+  menuLinks.classList.toggle("active");
 }
