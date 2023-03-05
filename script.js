095f10a4c18c32070c0319c5f1a03f057e5856f0
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-03-04 22:22:06 -0300

    change menu icon to close

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function openMenu() {
+let mobileMenu = document.querySelector(".menu-mobile-icon");
+let mobileIconImg = document.querySelector(".icon-menu-hamburger");
+
+mobileMenu.addEventListener("click", () => {
   let menuLinks = document.querySelector(".nav-menu");
   menuLinks.classList.toggle("active");
-}
+
+  // trocar ícone para o "X" se conter a classe active
+  if (menuLinks.classList.contains("active")) {
+    mobileIconImg.src = "assets/close_FILL0_wght400_GRAD0_opsz48.svg";
+  } else {
+    mobileIconImg.src = "assets/menu_FILL0_wght400_GRAD0_opsz48.svg";
+  }
+});
